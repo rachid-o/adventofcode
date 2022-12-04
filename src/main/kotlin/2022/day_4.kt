61e99ae0155968e0fa5ad8d70f8c3970a23c722c
Rachid <rachid@weareblox.com>
2022-12-04 08:29:51 +0100

    Day 4

---
+++ src/main/kotlin/2022/day_4.kt
@@ -0,0 +1,37 @@
+package `2022`
+
+import java.io.File
+
+fun main() {
+    val inputFilename = "day_4.txt"
+    println("Reading file $resourcePath/$inputFilename")
+    val input = File(resourcePath, inputFilename)
+        .readLines()
+        .map { pair ->
+            val (first, second) = pair.split(",")
+                .map { strRange ->
+                    val array = strRange.split("-")
+                    val range = array[0].toInt()..array[1].toInt()
+                    range.map { it }.toSet()
+                }
+            first to second
+        }
+
+    val answer1 = part1(input)
+    println("Answer 1: $answer1")
+
+    val answer2 = part2(input)
+    println("Answer 2: $answer2")
+}
+
+fun part1(input: List<Pair<Set<Int>, Set<Int>>>): Int {
+    return input.count { (first, second) ->
+        first.containsAll(second)  || second.containsAll(first)
+    }
+}
+
+fun part2(input: List<Pair<Set<Int>, Set<Int>>>): Int {
+    return input.count { (first, second) ->
+        first.any { second.contains(it) } || second.any { first.contains(it) }
+    }
+}
