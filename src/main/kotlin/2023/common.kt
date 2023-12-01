7f37f443c45fba36147687e68a9c6d27482c89e4
Rachid <rachid@weareblox.com>
2023-12-01 08:18:11 +0100

    Day 1 - 2023

---
+++ src/main/kotlin/2023/common.kt
@@ -0,0 +1,2 @@
+package `2023`
+const val resourcePath = "src/main/resources/2023"
---
+++ src/main/kotlin/2023/day_1.kt
@@ -0,0 +1,48 @@
+package `2023`
+
+import java.io.File
+
+fun main() {
+    val inputFilename = "day_1.txt"
+    println("Reading file $resourcePath/$inputFilename")
+    val input = File(resourcePath, inputFilename)
+        .readLines()
+    println("input: $input")
+
+    val answer1 = part1(input)
+    println("Answer 1: $answer1")
+
+    val answer2 = part2(input)
+    println("Answer 2: $answer2")
+    check(answer1 == 54951)
+    check(answer2 == 55218)
+}
+
+private fun part1(lines: List<String>) =
+    lines.sumOf { line ->
+        val digits = line.filter { it.isDigit() }
+        (digits.first().toString() + digits.last().toString()).toInt()
+    }
+
+val digitMap = mapOf(
+    "one" to "one1one",
+    "two" to "tw2wo",
+    "three" to "thre3hree",
+    "four" to "fou4our",
+    "five" to "fiv5ive",
+    "six" to "si6ix",
+    "seven" to "seve7even",
+    "eight" to "eigh8ight",
+    "nine" to "nin9ine"
+)
+
+private fun part2(lines: List<String>): Int {
+    val newLines = lines.map { line ->
+        var newLine = line
+        digitMap.entries.forEach { (word, digit) ->
+            newLine = newLine.replace(word, digit)
+        }
+        newLine
+    }
+    return part1(newLines)
+}
